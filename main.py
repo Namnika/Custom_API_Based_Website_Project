1e5e609e14c8f38a610eb3b32ad92a56cdc8c0d0
Namnika <git@github.com:Namnika/github-upload.git>
2022-07-06 16:19:26 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ login_manager.init_app(app)
 
 # *****CONNECT TO DB********
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///arts-album.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
